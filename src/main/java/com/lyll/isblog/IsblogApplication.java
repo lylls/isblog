44c6205eac0ecde52fbb56cfaebaf0c207e7487e
yong <yong3269@163.com>
2018-03-23 13:40:59 +0800

    上传项目

---
+++ src/main/java/com/lyll/isblog/IsblogApplication.java
@@ -0,0 +1,12 @@
+package com.lyll.isblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsblogApplication.class, args);
+	}
+}
